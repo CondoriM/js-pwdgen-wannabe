4d7bcf5784394130703ccf1631bfec70cf70e8da
CondoriM <condoria.miguel@gmail.com>
2022-03-23 16:49:13 +0100

    completed exercise

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,21 @@
 // creare reparto nome
 
+//chiedi nome utente
+let useName = prompt("Please enter your name");
+console.log(useName);
+document.getElementById('name_user').innerHTML = useName;
+
+//chiedi cognome utente
+let useSurname = prompt("Please enter your surname");
+console.log(useSurname);
+document.getElementById('sur_name').innerHTML = useSurname;
+
+//chiedi colore utente
+let useColor = prompt("Please enter your fav color");
+console.log(useColor);
+document.getElementById('color_user').innerHTML = useColor;
+
+const Password = useName + useSurname + useColor + 22;
+console.log(Password);
+
+document.getElementById('pas').innerHTML = Password;
